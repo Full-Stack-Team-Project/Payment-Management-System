1d96e46741c6f3561d0f6ea17ec652d74dd3e56d
levenmartinov <levkomartinov@gmail.com>
2025-05-22 18:33:53 +0100

    Added entity and repository layers

---
+++ bankapp/bankapp/src/main/java/com/bankteam/bankapp/entity/Transaction.java
@@ -0,0 +1,27 @@
+package com.bankteam.bankapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double amount;
+    private String type;
+    private LocalDateTime date;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+}
